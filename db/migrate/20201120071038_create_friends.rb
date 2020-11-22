1ae144424c1ac773e22133ca16b29464c3cd5675
Pulkit <Pulkitjndl@gmail.com>
2020-11-22 17:02:57 +0530

    init

---
+++ db/migrate/20201120071038_create_friends.rb
@@ -0,0 +1,14 @@
+class CreateFriends < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.string :last
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :twitter
+
+      t.timestamps
+    end
+  end
+end
